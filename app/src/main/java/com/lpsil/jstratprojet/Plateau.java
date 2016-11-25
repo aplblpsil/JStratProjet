681c8425de01548873bb1a2f4d8dc5eaf998c519
Leopoulpe <leopold.bodu@hotmail.fr>
2016-11-25 18:17:37 +0100

    implem plateau

--- app/src/main/java/com/lpsil/jstratprojet/Plateau.java
+++ app/src/main/java/com/lpsil/jstratprojet/Plateau.java
@@ -1,9 +1,20 @@
 package com.lpsil.jstratprojet;
 
+import java.util.ArrayList;
+
 /**
  * Created by Léo on 25/11/2016.
  */
 
 public class Plateau {
+    Personnage[][] grillePerso;
     boolean[][] grilleObstacle;
+    int[][] grilleDecor;
+    boolean[][] grilleDeplacement;
+    boolean[][] grilleAttaque;
+
+    ArrayList<Personnage> equipe_1;
+    ArrayList<Personnage> equipe_2;
+
+
 }
